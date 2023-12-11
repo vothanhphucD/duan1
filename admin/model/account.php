cea8632c7533d375e8a035f816aa34d826688169
hongnhanh177 <nhanhdhps32732@gpt.edu.vn>
2023-12-12 00:46:52 +0700

    cap nhat product

--- admin/model/account.php
+++ admin/model/account.php
@@ -3,5 +3,11 @@
         session_start();
     }
 
-
+    require_once 'connect.php';
+    class account extends dao_con {
+        public function show_account() {
+            $sql = "SELECT * FROM account ORDER BY account_id DESC";
+            return $this->conn_show_all($sql);
+        }
+    }
 ?>
