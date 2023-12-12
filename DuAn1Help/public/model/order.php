8b1108aedf8d6f74d00e3c2a36c6efd313f611e3
khang <truongcong08092k4@gmail.com>
2023-12-12 22:14:57 +0700

    Thêm vào đơn hang và thêm chi tiet dat hang và hiển thi đat hang trong model

--- DuAn1Help/public/model/order.php
+++ DuAn1Help/public/model/order.php
@@ -4,7 +4,20 @@
     }
 
     class order_lass extends dao_con {
-       
+        public function add_order($a, $b, $c) {
+            $sql = "INSERT INTO orders(order_total, order_pay, order_note, account_id) VALUES(?, ?, ?, ?)";
+            return $this->conn_execute($sql, $a, $b, $c, $_SESSION['x_user']['account_id']);
+        }
+        
+        public function add_order_details($a, $b, $c, $d, $e) {
+            $sql = "INSERT INTO order_details (details_name, details_price, details_img, details_qty, product_id, order_id) VALUES (?, ?, ? , ?, ?, (SELECT order_id FROM orders ORDER BY order_id DESC LIMIT 1))";
+            return $this->conn_execute($sql, $a, $b, $c, $d, $e);
+        }
+
+        public function show_order() {
+            $sql = "SELECT * FROM orders WHERE account_id = ?";
+            return $this->conn_show_all($sql, $_SESSION['x_user']['account_id']);
+        }
 
         public function showOrderDetails($a) {
             $sql = "SELECT * FROM order_details WHERE order_id = ?";
