30a08e8d1aa3e9e99fea24f0891f970f7cf8965c
khang <truongcong08092k4@gmail.com>
2023-12-13 23:24:49 +0700

    loi binh luan xoa

--- DuAn1Help/public/model/comment.php
+++ DuAn1Help/public/model/comment.php
@@ -6,19 +6,5 @@
         require "connect.php";
     }
 
-    class comment_lass extends dao_con {
-        public function addCmt($content, $star, $proId) {
-            $sql = "INSERT INTO rate(rate_comment, rate_star, product_id, account_id) VALUES(?, ?, ?, ?)";
-            return $this->conn_execute($sql, $content, $star, $proId, $_SESSION['x_user']['account_id']);
-        }
 
-        public function showCmt($a) {
-            $sql = "SELECT rate.*, account.*
-                    FROM rate
-                    JOIN account ON rate.account_id = account.account_id
-                    WHERE rate.product_id = ?
-            ";
-            return $this->conn_show_all($sql, $a);
-        }
-    }
 ?>
