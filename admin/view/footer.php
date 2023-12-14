110704d2595375fd04939c8972b61d7768eee9db
hongnhanh177 <nhanhdhps32732@gpt.edu.vn>
2023-12-15 00:48:54 +0700

    update

--- admin/view/footer.php
+++ admin/view/footer.php
@@ -1,8 +1,8 @@
 <footer class="footer">
     <div class="d-sm-flex justify-content-center justify-content-sm-between">
-        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © ps32791
+        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © DreamMakers
             2023<br>
-            Backend By <a href="https://www.facebook.com/nguyentany.2105" target="_blank">DreamMakers</a>
+            Backend By <a href="https://www.facebook.com/hongnhanh17777777" target="_blank">DreamMakers</a>
 
         </span>
         <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"><a href="https://ap.poly.edu.vn/" target="_blank">FPT Polytechnic</a> from
